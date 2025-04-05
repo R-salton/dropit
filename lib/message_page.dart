19742d2c7ecdbc38589577cab977258e69adf56d
R-salton <ringusa22@gmail.com>
2025-04-05 22:42:54 +0200

    Chat page

--- lib/message_page.dart
+++ lib/message_page.dart
@@ -1,3 +1,4 @@
+import 'package:dropit/core/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -36,24 +37,32 @@ class MessagePage extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                _buildRecentContact("John Doe",context),
-                _buildRecentContact("Salton",context),
-                _buildRecentContact("Karenzi",context),
-                _buildRecentContact("Sadev",context),
-                _buildRecentContact("Baraka",context),
-                _buildRecentContact("Paul",context),
+                _buildRecentContact("John Doe","https://picsum.photos/200",context),
+                _buildRecentContact("Salton","https://picsum.photos/100",context),
+                _buildRecentContact("Karenzi","https://picsum.photos/600",context),
+                _buildRecentContact("Sadev","https://picsum.photos/300",context),
+                _buildRecentContact("Baraka","https://picsum.photos/400",context),
+                _buildRecentContact("Paul","https://picsum.photos/500",context),
               ],
             ),
           ),
+          SizedBox(height: 10.h,),
         Expanded(child: Container(
+          decoration: BoxDecoration(
+            color: DefaultColors.messageListPage,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50.r),
+              topRight: Radius.circular(50.r),
+            ),
+          ),
           child: ListView(
           children: [
-            _buildMessageTile("Hi, I am John Doe","John Doe","10:00 AM",context),
-            _buildMessageTile("Hi, I am John Doe","John Doe","10:00 AM",context),
-          _buildMessageTile("Hello, I am John Doe","John Doe","10:00 AM",context),
-          _buildMessageTile("Hi, I am John Doe","John Doe","10:00 AM",context),
-            _buildMessageTile("I am John Doe","John Doe","10:00 AM",context),
-            _buildMessageTile("Hi, I am John Doe","John Doe","10:00 AM",context),
+            _buildMessageTile("Hi, I am John Doe","John Doe","https://picsum.photos/200","10:00 AM",context),
+            _buildMessageTile("Hi, I am Salton","Salton","https://picsum.photos/100","10:00 AM",context),
+            _buildMessageTile("Hi, I am Karenzi","Karenzi","https://picsum.photos/600","10:00 AM",context),
+            _buildMessageTile("Hi, I am Sadev","Sadev","https://picsum.photos/300","10:00 AM",context),
+            _buildMessageTile("Hi, I am Baraka","Baraka","https://picsum.photos/400","10:00 AM",context),
+            _buildMessageTile("Hi, I am Paul","Paul","https://picsum.photos/500","10:00 AM",context),
           ],          ),
         ))
         
@@ -64,13 +73,13 @@ class MessagePage extends StatelessWidget {
   }
 
 
-Widget _buildMessageTile (String message,String name,String time,BuildContext context) {
+Widget _buildMessageTile (String message,String name,String profilePicture,String time,BuildContext context) {
 
   return ListTile(
-    contentPadding: EdgeInsets.symmetric(horizontal: 20.w,vertical: 10.h),
+    contentPadding: EdgeInsets.symmetric(horizontal: 15.w,vertical: 10.h),
     leading: CircleAvatar(
       radius: 30.r,
-      backgroundImage: NetworkImage('https://picsum.photos/300'),
+      backgroundImage: NetworkImage(profilePicture),
 
     ),
     title: Text(name, style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
@@ -84,14 +93,14 @@ Widget _buildMessageTile (String message,String name,String time,BuildContext co
 }
  
 
-Widget _buildRecentContact(name,BuildContext context) {
+Widget _buildRecentContact(name, profiPicture,BuildContext context) {
   return Padding(padding: EdgeInsets.symmetric(horizontal: 10.w),
   child: Column(
 
     children: [
       CircleAvatar(
         radius: 30.r,
-        backgroundImage: NetworkImage('https://picsum.photos/200'),
+        backgroundImage: NetworkImage(profiPicture),
       ),
       SizedBox(height: 5.h),
 
