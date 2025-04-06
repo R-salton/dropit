e9d7b3824879dfaed3a31db72f3d077bac61c992
R-salton <ringusa22@gmail.com>
2025-04-06 22:07:05 +0200

    Chatting page

--- lib/core/chat_page.dart
+++ lib/core/chat_page.dart
@@ -33,8 +33,17 @@ class ChatPage extends StatelessWidget {
             padding: EdgeInsets.all(20.w),
             children: [
               _buildReceivedMessage(context,"Hi, how are you?"),
+              _buildSentMessage(context,"I am fine, thank you!"),
+              _buildReceivedMessage(context,"So what are you doing?"),
+              _buildSentMessage(context,"I am just coding"),
+              _buildReceivedMessage(context,"Seriously?, because you are a programmer?"),
+              _buildSentMessage(context,"Yes, I am"),
+              _buildReceivedMessage(context,"What do you do?"),
+              _buildSentMessage(context,"I am coding"),
+              _buildReceivedMessage(context,"Okay"),
             ]
-          ),)
+          ),),
+          _buildMessageInput(context,"Message..."),
         ],
       ),
     );
@@ -57,4 +66,68 @@ class ChatPage extends StatelessWidget {
       ),
     );
   }
+
+Widget _buildSentMessage(BuildContext context, String message) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Container(
+        margin: EdgeInsets.only(bottom: 5.h,top: 5.h,right: 20.w),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: DefaultColors.senderMessage,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20.w),bottomLeft: Radius.circular(20.w),bottomRight: Radius.circular(20..w),topRight: Radius.circular(0.w)),
+        ),
+        child: Text(
+          message,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      ),
+    );
+  }
+
+Widget _buildMessageInput(BuildContext context, String message) {
+    return Container(
+      margin: EdgeInsets.all(25.w),
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      decoration: BoxDecoration(
+        color: DefaultColors.sentMessageInput,
+        borderRadius: BorderRadius.circular(30.r),
+      ),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Icon(Icons.add_a_photo_outlined,color: Colors.grey,),
+          ),
+          SizedBox(width: 10.w,),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: message,
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+              style:TextStyle(color: Colors.white),
+            ),
+            
+          ),
+        
+                SizedBox(width: 10.w,),
+         
+          GestureDetector(
+            onTap: () {},
+            child: Icon(Icons.mic,color: Colors.grey,),
+          ),
+          SizedBox(width: 10.w,),
+          GestureDetector(
+            onTap: () {},
+            child: Icon(Icons.send,color: Colors.grey,),
+          ),
+         
+        ],
+      ),
+    );
+  }
+
 }
+
